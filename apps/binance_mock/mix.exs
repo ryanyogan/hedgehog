ea369c91741650a7bea397dc2c29ca3e99b30954
ryan <ryanyogan@gmail.com>
2021-05-14 10:47:38 -0500

    initial commit of mocked binance service

---
+++ apps/binance_mock/mix.exs
@@ -0,0 +1,28 @@
+defmodule BinanceMock.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :binance_mock,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {BinanceMock.Application, []}
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
